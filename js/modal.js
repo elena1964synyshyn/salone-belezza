fd1f316288801b5e61e748268f6dd249f9c7d0cf
elena <synyshyn1@gmail.com>
2026-02-03 18:02:53 +0100

    update madal.js

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
