048f90351650189448dadc6a50057ee8c4994bfa
elena <synyshyn1@gmail.com>
2026-03-02 21:07:03 +0100

    created folder scss and update main.min.css and scss

--- js/formats-slider.js
+++ js/formats-slider.js
@@ -1,26 +1,47 @@
-const swiper =new Swiper('.procedures-swiper', {
+// const swiper =new Swiper('.procedures-swiper', {
+//   loop: true,
+//   slidesPerView: 1,
+//   spaceBetween: 20,
+
+//   breakpoints: {
+//     768: { slidesPerView: 2 },
+//     1024: { slidesPerView: 3 },
+//   },
+
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+
+//   navigation: {
+//     nextEl: '.carousel-button__arrow--next',
+//     prevEl: '.carousel-button__arrow--prev',
+//   },
+// });
+  
+
+const swiper = new Swiper('.swiper-container', {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 20,
-
   breakpoints: {
-    768: { slidesPerView: 2 },
-    1024: { slidesPerView: 3 },
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
   },
-
   pagination: {
-    el: '.procedures-swiper .swiper-pagination',
-    clickable: true,
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
   },
-
   navigation: {
     nextEl: '.carousel-button__arrow--next',
     prevEl: '.carousel-button__arrow--prev',
   },
 });
-  
-
-
   
   
 
