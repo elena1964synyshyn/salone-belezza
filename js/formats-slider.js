d04945c4a00f22491920befd528ebf284740d8b8
elena <synyshyn1@gmail.com>
2026-03-12 21:18:31 +0100

    first commit

--- js/formats-slider.js
+++ js/formats-slider.js
@@ -1,23 +1,4 @@
-// const swiper =new Swiper('.procedures-swiper', {
-//   loop: true,
-//   slidesPerView: 1,
-//   spaceBetween: 20,
 
-//   breakpoints: {
-//     768: { slidesPerView: 2 },
-//     1024: { slidesPerView: 3 },
-//   },
-
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-
-//   navigation: {
-//     nextEl: '.carousel-button__arrow--next',
-//     prevEl: '.carousel-button__arrow--prev',
-//   },
-// });
   
 
 const swiper = new Swiper('.swiper-container', {
